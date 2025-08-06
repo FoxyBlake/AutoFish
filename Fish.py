9d932881f4dc3752afae2cd747acd3391f074949
FoxyBlake <blakeuwu6060@gmail.com>
2025-08-06 13:32:06 -0400

    Update Fish.py
    Clicking method bad

--- Fish.py
+++ Fish.py
@@ -7,30 +7,10 @@ import keyboard
 import random
 import win32api, win32con
 import time
-import ctypes
 
 #bar Pos 1 X:   930     Y: 1100
 #bar Pos 2 X:   1630     Y: 1100
 #Fishing Icon X: 1307 Y: 1268 RGB: (147, 111,  74)
-
-SendInput = ctypes.windll.user32.SendInput
-
-# Constants
-MOUSE_LEFTDOWN = 0x0002
-MOUSE_LEFTUP = 0x0004
-
-class MOUSEINPUT(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", ctypes.POINTER(ctypes.c_ulong))]
-
-class INPUT(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("mi", MOUSEINPUT)]
-
 
 def click(x,y):
     win32api.SetCursorPos((x,y))
@@ -49,13 +29,6 @@ def is_grey_pixel_present(region_start_x, y, width):
     
     return np.any(green_match)
 
-def send_fast_click():
-    extra = ctypes.c_ulong(0)
-    ii_ = INPUT(type=0, mi=MOUSEINPUT(0, 0, 0, MOUSE_LEFTDOWN, 0, ctypes.pointer(extra)))
-    SendInput(1, ctypes.pointer(ii_), ctypes.sizeof(ii_))
-    ii_.mi.dwFlags = MOUSE_LEFTUP
-    SendInput(1, ctypes.pointer(ii_), ctypes.sizeof(ii_))
-
 random_integer = random.randint(1, 10)
 
 while keyboard.is_pressed('q') == False:
@@ -69,8 +42,7 @@ while keyboard.is_pressed('q') == False:
     elif r != 242:
         if not is_grey_pixel_present(933, 1100, 694):
             for i in range(12):
-                send_fast_click()
-                #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-                #time.sleep(0.001)  # 1 ms (optional)
-                #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+                time.sleep(0.001)  # 1 ms (optional)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
     time.sleep(0.002)
